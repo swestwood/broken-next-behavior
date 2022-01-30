b84c5f0337523331f2ef79070206878fea51d39c
Sophia Westwood <swestwood92@gmail.com>
2022-01-30 00:47:49 -0800

    Demo of bad broken behavior

---
+++ pages/broken.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+
+const examples = ['Penguin', 'Sponges', 'Dog', 'Cat', 'Lizard', 'Rabbit', 'Rat', 'Fish',
+    'Duck', "Hamster", "Squirrel", "Buffalo", "Fox", "Wolf", "Bear"]
+
+const getRandomAnimal = () => {
+    const index = Math.floor(Math.random() * examples.length)
+    return examples[index]
+}
+
+const Broken : NextPage = () => {
+    const [animal, setAnimal] = useState(getRandomAnimal())
+    // const animal = getRandomAnimal()   // Doing this instead of useState above also breaks
+    // The only way to fix it is to not set it in useState, then set it later in useEffect.
+    const link = 'https://en.wikipedia.org/wiki/' + animal
+    return (
+        <div>
+            <div style={{marginTop: 32, marginLeft: 32}}>
+                <p>This is the animal: {animal}</p>
+                <p>This is the link: <a href={link}>{link}</a></p>
+                <p>But, when you click the link above, it goes to a different animal's page.</p>
+                <p>Hovering over the link shows that the <code> href</code> is different even though it is the same <code>link</code> variable being used.</p>
+                <p>If the link correctly matches, it is because you navigated to this page from the main page (or occasional random chance) -- if you refresh, it will be wrong again.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Broken
